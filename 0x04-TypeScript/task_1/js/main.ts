66eac97b3a11585f24e6d1a26ddbd957fb126e85
owolabenjade <owolabenjade@gmail.com>
2025-04-25 06:32:57 +0100

    typescript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,30 @@
+/**
+ * Teacher interface
+ */
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any; // Allow additional attributes
+  }
+  
+  // Example of creating a teacher
+  const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+  
+  console.log(teacher3);
+  
+  // Should print:
+  // Object
+  // contract: false
+  // firstName: "John"
+  // fullTimeEmployee: false
+  // lastName: "Doe"
+  // location: "London"
