22e2dcfdba0dcb9dbad0a88ad2d8f05f187b502b
Skilltechdev <skillxplorer@gmail.com>
2025-07-16 15:38:01 +0100

    progress

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -9,10 +9,13 @@ class StudentsController {
         let responseText = 'This is the list of our students';
 
         // Sort fields alphabetically (case insensitive)
-        const sortedFields = Object.keys(fields).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+        const sortedFields = Object.keys(fields).sort((a, b) => (
+          a.toLowerCase().localeCompare(b.toLowerCase())
+        ));
 
         sortedFields.forEach((field) => {
-          responseText += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+          const studentList = fields[field].join(', ');
+          responseText += `\nNumber of students in ${field}: ${fields[field].length}. List: ${studentList}`;
         });
 
         response.status(200).send(responseText);
