bc51d89e41515f6121327d24f6d320ec4ad6a1f6
Skilltechdev <skillxplorer@gmail.com>
2025-07-16 14:36:39 +0100

    progress

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -3,26 +3,26 @@ const fs = require('fs').promises;
 
 function countStudents(path) {
   return fs.readFile(path, 'utf8')
-    .then(data => {
-      const lines = data.split('\n').filter(line => line.trim() !== '');
+    .then((data) => {
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
       const students = lines.slice(1);
-      
-      let result = `Number of students: ${students.length}\n`;
-      
+
+      let result = `Number of students: ${students.length}`;
+
       const fields = {};
-      students.forEach(student => {
+      students.forEach((student) => {
         const [firstname, , , field] = student.split(',');
         if (!fields[field]) {
           fields[field] = [];
         }
         fields[field].push(firstname);
       });
-      
-      Object.keys(fields).forEach(field => {
-        result += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+
+      Object.keys(fields).forEach((field) => {
+        result += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
       });
-      
-      return result.trim();
+
+      return result;
     })
     .catch(() => {
       throw new Error('Cannot load the database');
@@ -32,17 +32,17 @@ function countStudents(path) {
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello ALX!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
   const databasePath = process.argv[2];
-  
+
   countStudents(databasePath)
-    .then(studentData => {
+    .then((studentData) => {
       res.send(`This is the list of our students\n${studentData}`);
     })
-    .catch(error => {
+    .catch((error) => {
       res.send(`This is the list of our students\n${error.message}`);
     });
 });
