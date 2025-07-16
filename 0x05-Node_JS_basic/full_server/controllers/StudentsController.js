5fd5abc975eca0ee44ffcfac094a29995840aee3
Skilltechdev <skillxplorer@gmail.com>
2025-07-16 14:55:13 +0100

    progress

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,39 +3,39 @@ import readDatabase from '../utils.js';
 class StudentsController {
   static getAllStudents(request, response) {
     const databasePath = process.argv[2];
-    
+
     readDatabase(databasePath)
-      .then(fields => {
-        let responseText = 'This is the list of our students\n';
-        
+      .then((fields) => {
+        let responseText = 'This is the list of our students';
+
         // Sort fields alphabetically (case insensitive)
-        const sortedFields = Object.keys(fields).sort((a, b) => 
-          a.toLowerCase().localeCompare(b.toLowerCase())
+        const sortedFields = Object.keys(fields).sort((a, b) =>
+          a.toLowerCase().localeCompare(b.toLowerCase()),
         );
-        
-        sortedFields.forEach(field => {
-          responseText += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+
+        sortedFields.forEach((field) => {
+          responseText += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
         });
-        
-        response.status(200).send(responseText.trim());
+
+        response.status(200).send(responseText);
       })
       .catch(() => {
         response.status(500).send('Cannot load the database');
       });
   }
-  
+
   static getAllStudentsByMajor(request, response) {
     const { major } = request.params;
-    
+
     if (major !== 'CS' && major !== 'SWE') {
       response.status(500).send('Major parameter must be CS or SWE');
       return;
     }
-    
+
     const databasePath = process.argv[2];
-    
+
     readDatabase(databasePath)
-      .then(fields => {
+      .then((fields) => {
         const students = fields[major] || [];
         response.status(200).send(`List: ${students.join(', ')}`);
       })
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -7,20 +7,20 @@ function readDatabase(filePath) {
         reject(new Error('Cannot load the database'));
         return;
       }
-      
+
       try {
-        const lines = data.split('\n').filter(line => line.trim() !== '');
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
         const students = lines.slice(1);
-        
+
         const fields = {};
-        students.forEach(student => {
+        students.forEach((student) => {
           const [firstname, , , field] = student.split(',');
           if (!fields[field]) {
             fields[field] = [];
           }
           fields[field].push(firstname);
         });
-        
+
         resolve(fields);
       } catch (error) {
         reject(new Error('Cannot load the database'));
