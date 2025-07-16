03ce0d892eb9202c8de27cdc7c9255b0b4f6dd94
Skilltechdev <skillxplorer@gmail.com>
2025-07-16 14:11:02 +0100

    progress

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,26 +3,32 @@ const fs = require('fs').promises;
 
 function countStudents(path) {
   return fs.readFile(path, 'utf8')
-    .then(data => {
-      const lines = data.split('\n').filter(line => line.trim() !== '');
+    .then((data) => {
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
       const students = lines.slice(1);
-      
-      let result = `Number of students: ${students.length}\n`;
-      
+
+      let result = `Number of students: ${students.length}`;
+
       const fields = {};
-      students.forEach(student => {
+      students.forEach((student) => {
         const [firstname, , , field] = student.split(',');
-        if (!fields[field]) {
-          fields[field] = [];
+        if (!fields[field] || field === undefined) {
+          if (field && field.trim() !== '') {
+            if (!fields[field]) {
+              fields[field] = [];
+            }
+            fields[field].push(firstname);
+          }
+        } else {
+          fields[field].push(firstname);
         }
-        fields[field].push(firstname);
       });
-      
-      Object.keys(fields).forEach(field => {
-        result += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+
+      Object.keys(fields).forEach((field) => {
+        result += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
       });
-      
-      return result.trim();
+
+      return result;
     })
     .catch(() => {
       throw new Error('Cannot load the database');
@@ -32,22 +38,22 @@ function countStudents(path) {
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  
+
   if (req.url === '/') {
-    res.end('Hello ALX!');
+    res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
     const databasePath = process.argv[2];
-    
+
     countStudents(databasePath)
-      .then(studentData => {
+      .then((studentData) => {
         res.end(studentData);
       })
-      .catch(error => {
+      .catch((error) => {
         res.end(error.message);
       });
   } else {
-    res.end('Hello ALX!');
+    res.end('Hello Holberton School!');
   }
 });
 
